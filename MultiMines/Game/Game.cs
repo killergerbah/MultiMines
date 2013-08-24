03e019fc0e03f0ac423befaa9407aadf70e6a962
Raphael-Joel Lim <kgerbil@gmail.com>
2013-08-23 23:18:18 -0700

    first commit

---
+++ MultiMines/Game/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiMines
+{
+    public class Game
+    {
+        public long Id { get; set; }
+
+        public Player[] Players { get; private set; }
+
+        public Game(Player[] players)
+        {
+            Id = new Random().Next();
+            Players = players;
+        }
+
+        public Game(long id, Player[] players)
+        {
+            Id = id;
+            Players = players;
+        }
+    }
+}
---
+++ MultiMines/Game/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiMines
+{
+    public class Player
+    {
+        public string ConnectionId { get; private set; }
+        public string Name { get; private set; }
+
+        public Player(string connectionId, string name)
+        {
+            ConnectionId = connectionId;
+            Name = name;
+        }
+    }
+}
